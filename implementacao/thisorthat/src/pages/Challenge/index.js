d7c6f1cf3ee44aacefad3c427558ac89501634d5
rafaTsu <rafael.tsutomu@ufms.br>
2023-11-25 14:59:49 -0400

    update: atualiza jogo

--- implementacao/thisorthat/src/pages/Challenge/index.js
+++ implementacao/thisorthat/src/pages/Challenge/index.js
@@ -1,35 +1,107 @@
-import styles from "./Challenge.module.css"
+import React, { useState, useEffect } from 'react'
+import { Gatos, Cachorros, Patos, Raposas, Quokkas, Cafe } from '../../services/api'
+import { useCategory } from '../../contexts/CategoryContext';
+
 import Navbar from "../../components/Navbar"
-import { useCategory } from "../../contexts/CategoryContext"
-
-import { useNavigate } from "react-router-dom"
-
+import styles from "./Challenge.module.css"
 
 function Challenge(){
-    const { categoriaSelecionada, setCategoriaSelecionada } = useCategory()
-    const history = useNavigate()
+  const [seconds, setSeconds] = useState(30);
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+      }, 1000);
 
-    function confirmar(){
-        if(!categoriaSelecionada){
-            return alert("Escolha uma categoria")
-        }
-        else{
-            history('/jogo')
-        }
-    }
+      return () => clearInterval(timer);
+    }, []);
+    const { categoriaSelecionada } = useCategory()
+    const [imageLinks, setImageLinks] = useState([]);
+    
+    useEffect(() => {
+        const fetchImages = async () => {
+          try {
+            if(categoriaSelecionada === "Gatos"){
+                const response1 = await Gatos.get('/');
+                const response2 = await Gatos.get('/');
+
+                const data1 = response1.data;
+                const data2 = response2.data;
+        
+                setImageLinks([data1[0].url, data2[0].url]);
+            }
+            else if(categoriaSelecionada === "Cachorros"){
+              const response1 = await Cachorros.get('/');
+              const response2 = await Cachorros.get('/');
+
+              const data1 = response1.data;
+              const data2 = response2.data;
+      
+              setImageLinks([data1.url, data2.url]);
+            }
+            else if(categoriaSelecionada === "Patos"){
+              const response1 = await Patos.get('/');
+              const response2 = await Patos.get('/');
+
+              const data1 = response1.data;
+              const data2 = response2.data;
+      
+              setImageLinks([data1.message, data2.message]);
+            }
+            else if(categoriaSelecionada === "Raposas"){
+              const response1 = await Raposas.get('/');
+              const response2 = await Raposas.get('/');
+
+              const data1 = response1.data;
+              const data2 = response2.data;
+      
+              setImageLinks([data1.image, data2.image]);
+            }
+            else if(categoriaSelecionada === "Quokkas"){
+              const response1 = await Quokkas.get('/');
+              const response2 = await Quokkas.get('/');
+
+              const data1 = response1.data;
+              const data2 = response2.data;
+      
+              setImageLinks([data1.image, data2.image]);
+            }
+            else if(categoriaSelecionada === "Cafe"){
+            const response1 = await Cafe.get('/');
+            const response2 = await Cafe.get('/');
+
+            const data1 = response1.data;
+            const data2 = response2.data;
+    
+            setImageLinks([data1.file, data2.file]);
+            }
+          } catch (error) {
+            console.error('Erro ao buscar imagens:', error);
+          }
+        };
+    
+      fetchImages();
+    }, [categoriaSelecionada]);
 
     return(
-        <div>
-            <Navbar />
+        <div>  
+            <Navbar />      
             <div className={styles.container}>
-                <div className={styles.container_opcao}>
-                    <button className={styles.button} onClick={(e)=>setCategoriaSelecionada("Nome - Categoria")}>Gatos</button>
-                </div>
-                <button className={styles.button_confirm} onClick={confirmar} >Começar</button>
-                <strong className={styles.estilo}>Categoria selecionado: {categoriaSelecionada}</strong>
-            </div>
+            {imageLinks.map((link, index) => (
+            <button className={styles.button}><img key={index} src={link} alt={`Animal ${index + 1}`} className={styles.card} /></button>
+            ))}
+
         </div>
+            <div className={styles.timer}>
+              
+                <h1>{seconds}s</h1>
+                {seconds === 0 && <p>Tempo esgotado!</p>}
+          
+            </div>
+           <button className={styles.nextbutton} onClick={() => window.location.reload(false)}>Próximo</button>
+        </div>
+        
     )
+    
 }
 
 export default Challenge
--- implementacao/thisorthat/src/pages/Game/index.js
+++ implementacao/thisorthat/src/pages/Game/index.js
@@ -1,115 +1,98 @@
 import React, { useState, useEffect } from 'react'
 import { Gatos, Cachorros, Patos, Raposas, Quokkas, Cafe } from '../../services/api'
 import { useCategory } from '../../contexts/CategoryContext';
+import { db } from '../../services/firebaseConfig'
 
 import Navbar from "../../components/Navbar"
 import styles from "./Game.module.css"
 
 function Game(){
-  const [seconds, setSeconds] = useState(30);
-    useEffect(() => {
-      const timer = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds - 1);
-      }, 1000);
-
-      return () => clearInterval(timer);
-    }, []);
-    const { categoriaSelecionada } = useCategory()
-    const [imageLinks, setImageLinks] = useState([]);
+  const [imagemEscolhida, setImagemEscolhida] = useState('')
+  const { categoriaSelecionada } = useCategory()
+  const [imageLinks, setImageLinks] = useState([]);
+  
+  async function handleChoice(e){
+    e.preventDefault()
     
-    useEffect(() => {
-        const fetchImages = async () => {
-          try {
-            if(categoriaSelecionada === "Gatos"){
-                const response1 = await Gatos.get('/');
-                const response2 = await Gatos.get('/');
 
-                const data1 = response1.data;
-                const data2 = response2.data;
+    window.location.reload(false)
+  }
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        if(categoriaSelecionada === "Gatos"){
+          const response1 = await Gatos.get('/');
+          const response2 = await Gatos.get('/');
+
+          const data1 = response1.data;
+          const data2 = response2.data;
         
-                setImageLinks([data1[0].url, data2[0].url]);
-            }
-            else if(categoriaSelecionada === "Cachorros"){
-              const response1 = await Cachorros.get('/');
-              const response2 = await Cachorros.get('/');
+          setImageLinks([data1[0].url, data2[0].url]);
+        }
+        else if(categoriaSelecionada === "Cachorros"){
+          const response1 = await Cachorros.get('/');
+          const response2 = await Cachorros.get('/');
 
-              const data1 = response1.data;
-              const data2 = response2.data;
+          const data1 = response1.data;
+          const data2 = response2.data;
       
-              setImageLinks([data1.url, data2.url]);
-            }
-            else if(categoriaSelecionada === "Patos"){
-              const response1 = await Patos.get('/');
-              const response2 = await Patos.get('/');
+          setImageLinks([data1.url, data2.url]);
+        }
+        else if(categoriaSelecionada === "Patos"){
+          const response1 = await Patos.get('/');
+          const response2 = await Patos.get('/');
 
-              const data1 = response1.data;
-              const data2 = response2.data;
+          const data1 = response1.data;
+          const data2 = response2.data;
       
-              setImageLinks([data1.message, data2.message]);
-            }
-            else if(categoriaSelecionada === "Raposas"){
-              const response1 = await Raposas.get('/');
-              const response2 = await Raposas.get('/');
+          setImageLinks([data1.message, data2.message]);
+        }
+        else if(categoriaSelecionada === "Raposas"){
+          const response1 = await Raposas.get('/');
+          const response2 = await Raposas.get('/');
 
-              const data1 = response1.data;
-              const data2 = response2.data;
+          const data1 = response1.data;
+          const data2 = response2.data;
       
-              setImageLinks([data1.image, data2.image]);
-            }
-            else if(categoriaSelecionada === "Quokkas"){
-              const response1 = await Quokkas.get('/');
-              const response2 = await Quokkas.get('/');
+          setImageLinks([data1.image, data2.image]);
+        }
+        else if(categoriaSelecionada === "Quokkas"){
+          const response1 = await Quokkas.get('/');
+          const response2 = await Quokkas.get('/');
 
-              const data1 = response1.data;
-              const data2 = response2.data;
+          const data1 = response1.data;
+          const data2 = response2.data;
       
-              setImageLinks([data1.image, data2.image]);
-            }
-            else if(categoriaSelecionada === "Cafe"){
-            const response1 = await Cafe.get('/');
-            const response2 = await Cafe.get('/');
+          setImageLinks([data1.image, data2.image]);
+        }
+        else if(categoriaSelecionada === "Cafe"){
+          const response1 = await Cafe.get('/');
+          const response2 = await Cafe.get('/');
 
-            const data1 = response1.data;
-            const data2 = response2.data;
+          const data1 = response1.data;
+          const data2 = response2.data;
     
-            setImageLinks([data1.file, data2.file]);
-            }
-          } catch (error) {
-            console.error('Erro ao buscar imagens:', error);
-          }
-        };
+          setImageLinks([data1.file, data2.file]);
+        }
+      } catch (error) {
+        console.error('Erro ao buscar imagens:', error);
+      }
+    };
     
-      fetchImages();
-    }, [categoriaSelecionada]);
+    fetchImages();
+  }, [categoriaSelecionada]);
 
-    return(
-        <div>  
-            <Navbar />      
-            <div className={styles.container}>
-            {imageLinks.map((link, index) => (
-            <button className={styles.button}><img key={index} src={link} alt={`Animal ${index + 1}`} className={styles.card} /></button>
-            ))}
-
+  return(
+    <div>  
+      <Navbar />      
+        <div className={styles.container}>
+          <img src={imageLinks[0]} alt="Animal 1" className={styles.card} onClick={(e)=>{setImagemEscolhida(imageLinks[0])}}/>
+          <img src={imageLinks[1]} alt="Animal 2" className={styles.card} onClick={(e)=>{setImagemEscolhida(imageLinks[1])}}/>
         </div>
-            <div className={styles.timer}>
-              
-                <h1>{seconds}s</h1>
-                {seconds === 0 && <p>Tempo esgotado!</p>}
-          
-            </div>
-           <button className={styles.nextbutton} onClick={() => window.location.reload(false)}>Próximo</button>
-        </div>
-        
-    )
-              
-               
-
-
-
-
-
-
-
+           <button className={styles.nextbutton} onClick={handleChoice}>Próximo</button>
+        </div>      
+  )
 }
 
 export default Game

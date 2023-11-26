06e8566698f8ebc978ad546c4eb4cd96909a8711
rafaTsu <rafael.tsutomu@ufms.br>
2023-11-26 04:36:02 -0400

    feat: adiciona jogo

--- implementacao/thisorthat/src/pages/Challenge/index.js
+++ implementacao/thisorthat/src/pages/Challenge/index.js
@@ -1,46 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import { Gatos, Cachorros, Raposas, Quokkas } from '../../services/api';
 import { useCategory } from '../../contexts/CategoryContext';
-import Navbar from '../../components/Navbar';
-import styles from './Challenge.module.css';
+import { collection, getDocs, updateDoc, getDoc, doc } from "firebase/firestore";
+import { db } from '../../services/firebaseConfig';
+import Navbar from "../../components/Navbar";
+import styles from "./Challenge.module.css";
 
-function Game() {
+function Challenge() {
   const [imagemEscolhida, setImagemEscolhida] = useState('');
   const { categoriaSelecionada } = useCategory();
-  const [imageLinks, setImageLinks] = useState([]);
+  const [imageLinks1, setImageLinks1] = useState('');
+  const [imageLinks2, setImageLinks2] = useState('');
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
   const [seconds, setSeconds] = useState(10); 
   const [timerExpired, setTimerExpired] = useState(false); 
 
-  async function handleChoice(e) {
-    e.preventDefault();
-    window.location.reload(false);
+
+  function reiniciarPagina() {
+    window.location.reload();
   }
 
-  useEffect(() => {
-    const fetchImages = async () => {
+  const verificarID = async (id) => {
+    const queryRef = doc(db, "imagem", id);
+    const querySnapshot = await getDoc(queryRef);
+    if (!querySnapshot.exists()) {
       try {
-        if (categoriaSelecionada === 'Gatos') {
-          const response1 = await Gatos.get('/');
-          const response2 = await Gatos.get('/');
-          const data1 = response1.data;
-          const data2 = response2.data;
-          setImageLinks([data1[0].url, data2[0].url]);
-        } else if (categoriaSelecionada === 'Cachorros') {
-          const response1 = await Cachorros.get('/');
-          const response2 = await Cachorros.get('/');
-          const data1 = response1.data;
-          const data2 = response2.data;
-          setImageLinks([data1.url, data2.url]);
-        }
+        await updateDoc(queryRef, {
+          url: `https://cdn2.thecatapi.com/images/${id}.jpg`,
+          contagem: 0
+        });
+
       } catch (error) {
-        console.error('Erro ao buscar imagens:', error);
+        console.error("erro" + error);
       }
-    };
+    }
+  };
 
-    fetchImages();
-  }, [categoriaSelecionada]);
+  const buscarImagem = async () => {
+    try {
+      const response1 = await fetch('https://api.thecatapi.com/v1/images/search');
+      const response2 = await fetch('https://api.thecatapi.com/v1/images/search');
 
-  // Implementação do timer
+      const data1 = await response1.json();
+      const data2 = await response2.json();
+
+      const imageUrl1 = data1[0].url;
+      const imageUrl2 = data2[0].url;
+
+      setImageLinks1(imageUrl1);
+      setImageLinks2(imageUrl2);
+
+      const id1 = imageUrl1.split('/').pop();
+      const id2 = imageUrl2.split('/').pop();
+
+      verificarID(id1);
+      verificarID(id2);
+
+      const doc1 = await getDoc(doc(db, "imagem", id1));
+      const doc2 = await getDoc(doc(db, "imagem", id2));
+
+      const contagem1 = doc1.data().contagem;
+      const contagem2 = doc2.data().contagem;
+
+      const totalContagem = contagem1 + contagem2;
+
+      setPercentage1((contagem1 / totalContagem) * 100);
+      setPercentage2((contagem2 / totalContagem) * 100);
+
+    } catch (error) {
+      console.error('Erro na requisição:', error);
+    }
+  };
+
   useEffect(() => {
     const timer = setInterval(() => {
       if (seconds > 0) {
@@ -57,25 +88,80 @@ function Game() {
     };
   }, [seconds]);
 
+  async function updateURLCount(id) {
+    try {
+      const imagemRef = doc(db, "imagem", id);
+      const querySnapshot = await getDoc(imagemRef);
+  
+      if (querySnapshot.exists()) {
+        const contagem = querySnapshot.data().contagem || 0;
+  
+        await updateDoc(imagemRef, { contagem: contagem + 1 });
+      } else {
+        // Handle the case where the document does not exist
+        console.error("Document does not exist for id:", id);
+      }
+    } catch (error) {
+      console.error('Erro ao atualizar contagem:', error);
+    }
+  }
+
+  useEffect(() => {
+    if (categoriaSelecionada === "Gatos") {
+      buscarImagem();
+    } else if (categoriaSelecionada === "Cachorros") {
+      // Implement fetching dog images if needed
+    }
+  }, [categoriaSelecionada]);
+
+  const handleImageClick = (selectedImage) => {
+    setImagemEscolhida(selectedImage);
+    updateURLCount(selectedImage.split('/').pop());
+  };
+
   return (
     <div>
       <Navbar />
-      <div><p className={styles.pergunta}>Qual você prefere?</p></div>
+      <div>
+        <p className={styles.quest}>Qual você prefere?</p>
+      </div>
       <div className={styles.container}>
-        <img
-          src={imageLinks[0]} alt="Animal 1" className={styles.card} onClick={(e) => {setImagemEscolhida(imageLinks[0]);}}/>
+        <div style={{ position: 'relative', textAlign: 'center' }}>
+          <img
+            src={imageLinks1}
+            alt="Animal 1"
+            className={styles.card}
+            onClick={() => handleImageClick(imageLinks1)}
+          />
+          {imagemEscolhida === imageLinks1 && (
+            <p className={styles.percentage}>
+              {percentage1.toFixed(2)}%
+            </p>
+          )}
+        </div>
         <p className={styles.text}>OU</p>
-        <img src={imageLinks[1]} alt="Animal 2" className={styles.card} onClick={(e) => { setImagemEscolhida(imageLinks[1]);}}/>
+        <div style={{ position: 'relative', textAlign: 'center' }}>
+          <img
+            src={imageLinks2}
+            alt="Animal 2"
+            className={styles.card}
+            onClick={() => handleImageClick(imageLinks2)}
+          />
+          {imagemEscolhida === imageLinks2 && (
+            <p className={styles.percentage}>
+              {percentage2.toFixed(2)}%
+            </p>
+          )}
+        </div>
       </div>
       <div className={styles.timer}>
-        <h1>{seconds}</h1>
-        {timerExpired && <p className={styles.textTime}>TEMPO ESGOTADO!</p>}
+        {timerExpired ? (<p className={styles.textTime}>TEMPO ESGOTADO!</p>) : (<h1 className={styles.textTime}>{seconds}</h1>)}
       </div>
-      <button className={styles.nextbutton} onClick={handleChoice}>
+      <button className={styles.nextbutton} onClick={() => { reiniciarPagina() }}>
         Próximo
       </button>
     </div>
   );
 }
 
-export default Game
+export default Challenge;
--- implementacao/thisorthat/src/pages/Game/index.js
+++ implementacao/thisorthat/src/pages/Game/index.js
@@ -1,136 +1,145 @@
 import React, { useState, useEffect } from 'react'
 import { Gatos, Cachorros } from '../../services/api'
 import { useCategory } from '../../contexts/CategoryContext';
-import { collection, query, where, getDocs, addDoc, updateDoc, getDoc, setDoc, doc } from "firebase/firestore";
-import { db } from '../../services/firebaseConfig'
+import { collection, getDocs, updateDoc, getDoc, doc } from "firebase/firestore";
+import { db } from '../../services/firebaseConfig';
+import Navbar from "../../components/Navbar";
+import styles from "./Game.module.css";
 
-import Navbar from "../../components/Navbar"
-import styles from "./Game.module.css"
-
-function Game(){
-  const [imagemEscolhida, setImagemEscolhida] = useState('')
-  const { categoriaSelecionada } = useCategory()
-  const [imageLinks1, setImageLinks1] = useState();
-  const [imageLinks2, setImageLinks2] = useState();
+function Game() {
+  const [imagemEscolhida, setImagemEscolhida] = useState('');
+  const { categoriaSelecionada } = useCategory();
+  const [imageLinks1, setImageLinks1] = useState('');
+  const [imageLinks2, setImageLinks2] = useState('');
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
 
   function reiniciarPagina() {
     window.location.reload();
   }
 
-  const verificarID_gato = async (id) => {
-    const queryRef = doc(db, "imagem", id)
+  const verificarID = async (id) => {
+    const queryRef = doc(db, "imagem", id);
     const querySnapshot = await getDoc(queryRef);
     if (!querySnapshot.exists()) {
-      try{
-        await setDoc(queryRef, {
+      try {
+        await updateDoc(queryRef, {
           url: `https://cdn2.thecatapi.com/images/${id}.jpg`,
           contagem: 0
-        })
-      } catch(error){
-        console.error("erro" + error)
+        });
+      } catch (error) {
+        console.error("erro" + error);
       }
     }
   };
 
-  const verificarID_cachorro = async (id) => {
-    const queryRef = doc(db, "imagem", id)
-    const querySnapshot = await getDoc(queryRef);
-    if (!querySnapshot.exists()) {
-      try{
-        await setDoc(queryRef, {
-          url: `https://cdn2.thedogapi.com/images/${id}.jpg`,
-          contagem: 0
-        })
-      } catch(error){
-        console.error("erro" + error)
-      }
-    }
-  };
-  
-  const buscarImagemGato = async () => {
+  const buscarImagem = async () => {
     try {
-      const response1 = await Gatos.get('/')
-      const response2 = await Gatos.get('/')
+      const response1 = await fetch('https://api.thecatapi.com/v1/images/search');
+      const response2 = await fetch('https://api.thecatapi.com/v1/images/search');
 
-      const imageUrl = response1.data[0].url
-      const imageUr2 = response2.data[0].url
-      
-      const id1 = response1.data[0].id
-      const id2 = response2.data[0].id
+      const data1 = await response1.json();
+      const data2 = await response2.json();
 
-      setImageLinks1(imageUrl)
-      setImageLinks2(imageUr2)
+      const imageUrl1 = data1[0].url;
+      const imageUrl2 = data2[0].url;
 
-      verificarID_gato(id1)
-      verificarID_gato(id2)
+      setImageLinks1(imageUrl1);
+      setImageLinks2(imageUrl2);
+
+      const id1 = imageUrl1.split('/').pop();
+      const id2 = imageUrl2.split('/').pop();
+
+      verificarID(id1);
+      verificarID(id2);
+
+      const doc1 = await getDoc(doc(db, "imagem", id1));
+      const doc2 = await getDoc(doc(db, "imagem", id2));
+
+      const contagem1 = doc1.data().contagem;
+      const contagem2 = doc2.data().contagem;
+
+      const totalContagem = contagem1 + contagem2;
+
+      setPercentage1((contagem1 / totalContagem) * 100);
+      setPercentage2((contagem2 / totalContagem) * 100);
 
     } catch (error) {
       console.error('Erro na requisição:', error);
     }
   };
 
-  const buscarImagemCachorro = async () => {
-    try{
-      const response1 = await Cachorros.get('/')
-      const response2 = await Cachorros.get('/')
-
-      const imageUrl = response1.data[0].url
-      const imageUr2 = response2.data[0].url
-      
-      const id1 = response1.data[0].id
-      const id2 = response2.data[0].id
-
-      setImageLinks1(imageUrl)
-      setImageLinks2(imageUr2)
-
-      verificarID_cachorro(id1)
-      verificarID_cachorro(id2)
-
-    } catch(error){
-
-    }
-  }
-
-  async function updateURLCount() {
-    const imagemRef = collection(db, "imagem");
-    const q = query(imagemRef, where("url", "==", `${imagemEscolhida}`));
+  async function updateURLCount(id) {
+    try {
+      const imagemRef = doc(db, "imagem", id);
+      const querySnapshot = await getDoc(imagemRef);
   
-    const querySnapshot = await getDocs(q);
+      if (querySnapshot.exists()) {
+        const contagem = querySnapshot.data().contagem || 0;
   
-    for (const doc of querySnapshot.docs) {
-      try {
-        const documento = await getDoc(doc.ref);
-        const contagem = documento.data().contagem;
-  
-        await updateDoc(doc.ref, { contagem: contagem + 1 });
-      } catch (error) {
-        console.error('Erro ao atualizar contagem:', error);
+        await updateDoc(imagemRef, { contagem: contagem + 1 });
+      } else {
+        // Handle the case where the document does not exist
+        console.error("Document does not exist for id:", id);
       }
+    } catch (error) {
+      console.error('Erro ao atualizar contagem:', error);
     }
-    reiniciarPagina()
   }
 
   useEffect(() => {
-    if(categoriaSelecionada === "Gatos"){
-      buscarImagemGato();
+    if (categoriaSelecionada === "Gatos") {
+      buscarImagem();
+    } else if (categoriaSelecionada === "Cachorros") {
+      // Implement fetching dog images if needed
     }
-    else if(categoriaSelecionada === "Cachorros"){
-      buscarImagemCachorro();
-    }
-  }, [])
+  }, [categoriaSelecionada]);
 
-  return(
-    <div>  
-      <Navbar />      
-      <div><p className={styles.quest}>Qual você prefere?</p></div>
-        <div className={styles.container}>
-          <img src={imageLinks1} alt="Animal 1" className={styles.card} onClick={(e)=>{setImagemEscolhida(imageLinks1)}}/>
-          <p className={styles.text}>OU</p>
-          <img src={imageLinks2} alt="Animal 2" className={styles.card} onClick={(e)=>{setImagemEscolhida(imageLinks2)}}/>
+  const handleImageClick = (selectedImage) => {
+    setImagemEscolhida(selectedImage);
+    updateURLCount(selectedImage.split('/').pop());
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <p className={styles.quest}>Qual você prefere?</p>
+      </div>
+      <div className={styles.container}>
+        <div style={{ position: 'relative', textAlign: 'center' }}>
+          <img
+            src={imageLinks1}
+            alt="Animal 1"
+            className={styles.card}
+            onClick={() => handleImageClick(imageLinks1)}
+          />
+          {imagemEscolhida === imageLinks1 && (
+            <p className={styles.percentage}>
+              {percentage1.toFixed(2)}%
+            </p>
+          )}
         </div>
-        <button className={styles.nextbutton} onClick={(updateURLCount)}>Próximo</button>
-    </div>      
-  )
+        <p className={styles.text}>OU</p>
+        <div style={{ position: 'relative', textAlign: 'center' }}>
+          <img
+            src={imageLinks2}
+            alt="Animal 2"
+            className={styles.card}
+            onClick={() => handleImageClick(imageLinks2)}
+          />
+          {imagemEscolhida === imageLinks2 && (
+            <p className={styles.percentage}>
+              {percentage2.toFixed(2)}%
+            </p>
+          )}
+        </div>
+      </div>
+      <button className={styles.nextbutton} onClick={() => { reiniciarPagina() }}>
+        Próximo
+      </button>
+    </div>
+  );
 }
 
-export default Game
+export default Game;
